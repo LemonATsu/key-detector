38f539cfa6c9ad87d8114ce91cd4651ff4b65313
LemonAtSu <at_7788546@hotmail.com>
2016-04-08 11:30:03 +0800

    add feature extraction, logCompression, and sum pooling

--- Q1.py
+++ Q1.py
@@ -1,6 +1,18 @@
 import numpy as np
-import librosa
-
+import gtz, librosa
+import feature as feat
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    print('main')
+    clips = gtz.readAllClips(n=1)
+    print(len(clips))
+    f = feat.extractChroma(clips[0][0]) 
+    print(f)
+    comp = (feat.logCompression(f, 10))
+    plt.figure()
+    librosa.display.specshow(f, y_axis='chroma', x_axis='time')
+    plt.colorbar()
+    plt.tight_layout()
+    plt.show()
+    librosa.display.specshow(comp, y_axis='chroma', x_axis='time')
+    plt.show()
---
+++ feature.py
@@ -0,0 +1,13 @@
+import librosa
+import numpy as np
+
+def pooling(y, axis=1):
+    return np.sum(y, axis=axis)
+
+def logCompression(y, r):
+    logChroma = np.log10(1 + r * y)
+    return logChroma
+
+def extractChroma(y, sr=22050, w=2048, h=512):
+    chroma = librosa.feature.chroma_stft(y, sr, n_fft=w, hop_length=h)
+    return chroma 
