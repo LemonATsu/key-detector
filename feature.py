fc17471455626e14d4a60150d3b1f035a518fe26
LemonAtSu <at_7788546@hotmail.com>
2016-04-14 19:00:39 +0800

    fix a minor bug that log compression should take np.abs

--- feature.py
+++ feature.py
@@ -5,9 +5,9 @@ def pooling(y, axis=1):
     return np.sum(y, axis=axis)
 
 def logCompression(y, r):
-    logChroma = np.log10(1 + r * y)
+    logChroma = np.log10(1 + r * np.abs(y))
     return logChroma
 
-def extractChroma(y, sr=22050, w=2048, h=512):
+def extractChroma(y, sr=22050, w=2048, h=1024):
     chroma = librosa.feature.chroma_stft(y, sr, n_fft=w, hop_length=h)
     return chroma 
