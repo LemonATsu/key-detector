dab3d1f86cb2116bea009bc9c163bcfdf9bc9f20
LemonAtSu <at_7788546@hotmail.com>
2016-04-17 19:21:01 +0800

    Rewrite the log compression part

--- feature.py
+++ feature.py
@@ -6,8 +6,16 @@ def pooling(y, axis=1):
 
 def logCompression(y, r):
     logChroma = np.log10(1 + r * np.abs(y))
+    #print (y.shape)
+    #print (logChroma.shape)
     return logChroma
 
-def extractChroma(y, sr=22050, w=2048, h=1024):
-    chroma = librosa.feature.chroma_stft(y, sr, n_fft=w, hop_length=h)
+def extractChroma(y, sr=22050, w=2048, h=1024, r=0):
+    pitch = librosa.core.piptrack(y=y, sr=sr, n_fft=w, hop_length=h)[0]
+
+    if r > 0:
+        pitch = logCompression(pitch, r)
+
+    #chroma = librosa.feature.chroma_stft(y, sr, n_fft=w, hop_length=h)
+    chroma = librosa.feature.chroma_stft(S=pitch, sr=sr, n_fft=w, hop_length=h)
     return chroma 
